692550558f0e1112d7aa3aedda88c26127fc0b07
Tristin Barnett <tristinbarnett@gmail.com>
2020-05-29 15:51:05 -0700

    Updates

---
+++ precache-manifest.5a13cc45d1b4d65101b5ac835489d93e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fafa35397cdf8875cff2b25ea0ff1df",
+    "url": "/tristinbarnett/week19employeeTracker/index.html"
+  },
+  {
+    "revision": "f58535864251db539e9f",
+    "url": "/tristinbarnett/week19employeeTracker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e828f8129b1787805eb4",
+    "url": "/tristinbarnett/week19employeeTracker/static/js/2.f436039f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tristinbarnett/week19employeeTracker/static/js/2.f436039f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f58535864251db539e9f",
+    "url": "/tristinbarnett/week19employeeTracker/static/js/main.864840ee.chunk.js"
+  },
+  {
+    "revision": "2ac2546464d2ff41ced7",
+    "url": "/tristinbarnett/week19employeeTracker/static/js/runtime-main.49d6ea6f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tristinbarnett/week19employeeTracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
